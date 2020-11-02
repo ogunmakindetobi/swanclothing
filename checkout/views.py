c494f15905bdaf37848bd0211c1c5f072427ef35
ogunmakindetobi <ogunmakindetobi@outlook.com>
2020-11-02 17:23:18 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hj5DlFNmzmrm6WBPrFMfljERBKPJlCuPk8xkEwnbYPzD19tPDHDwdENf2AcRnJYBUi1xeYpAMAk7VOLKEqvPmB300Mn2aJXlw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
