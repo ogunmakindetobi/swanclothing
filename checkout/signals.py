e96ce43a0bbe6bfa2a953a0abcd4fa91179debb1
ogunmakindetobi <ogunmakindetobi@outlook.com>
2020-11-02 15:39:29 +0000

    added checkout view and templements

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,20 +1,18 @@
-from django.db.Models.signals import post_save,post_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, intance, created, **kwargs):
-    ***
-    Update order totel on Timeline update/created
-    ***
-
-    isinstance.order.update_total()
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, intance, **kwargs):
-    ***
-    Update order totel on Timeline delete
-    ***
-
-    isinstance.order.update_total()
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
