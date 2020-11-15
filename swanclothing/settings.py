b1d8e7d3addd558cf72fde124dbb7bde8e2d3079
ogunmakindetobi <ogunmakindetobi@outlook.com>
2020-11-15 16:00:50 +0000

    product delete view

--- swanclothing/settings.py
+++ swanclothing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9m$rz4r&(j9$%+2^%z(8_s+2spd!5@wj!8ty_p3rkn1-e-5*-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  True
 
 ALLOWED_HOSTS = []
 
